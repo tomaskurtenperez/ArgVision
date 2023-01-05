b2b0deaa853f11661f068283db18f76b44a3f172
tomaskurtenperez <117606826+tomaskurtenperez@users.noreply.github.com>
2023-01-04 22:35:14 -0300

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,22 @@
+document.write(`
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container-fluid">
+  
+    <a class="navbar-brand" href="index.html"><img src="imagenes/arvisionicon2.png" alt="logo" style="width: 20%;">ArgVision</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
+            
+            <a class="nav-link" href="nosotros.html">Nosotros</a>
+            <a class="nav-link" href="links.html">Links</a>
+            <a class="nav-link" href="contacto.html">Contacto</a>
+            <a class="nav-link" href="expo.html">Exposiciones</a>
+        </div>
+    </div>
+</div>
+</nav>
+
+`);
